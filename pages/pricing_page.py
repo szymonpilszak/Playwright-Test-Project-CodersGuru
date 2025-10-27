9b278260a3eaf5851a4611edb683fe5268b2544e
szymonpilszak <szymon.pilszak@gmail.com>
2025-10-27 17:54:51 +0100

    Add some docstring comments

--- pages/pricing_page.py
+++ pages/pricing_page.py
@@ -3,9 +3,7 @@ from pages.base_page import BasePage
 class PricingPage(BasePage):
     
     PRICING_MENU = "//div[@class='header__menu-container']//a[@href='/cennik']"
-
     PAGE_HEADER = "//h1[contains(@class, 'pricing__header')]"
-
     CONSULTATION_PRICE = "//h2[contains(@class, 'pricing__subheader--green')]"
 
     def open_pricing_page(self):
--- tests/test_login.py
+++ tests/test_login.py
@@ -1,3 +1,14 @@
+"""
+Login tests for the tester.codersguru.pl application.
+
+This module contains functional tests for the login functionality using
+the Page Object Pattern with Playwright and pytest
+
+Tests include:
+- Successful login (test_login_positive)
+- Invalid login attempts with parameterization (test_login_invalid)
+"""
+
 import pytest
 from pages.home_page import HomePage
 from pages.login_page import LoginPage
@@ -5,7 +16,18 @@ from pages.dashboard_page import DashboardPage
 
 base_url = "https://tester.codersguru.pl/"
 
-def test_login_positive(page):
+
+
+def test_login_positive(page): 
+    """
+    Test a successful login.
+
+            Steps:
+            1. Navigate to the home page.
+            2. Go to the login page.
+            3. Enter valid credentials.
+            4. Assert that the user is logged in successfully.
+    """
 
     home_page = HomePage(page)
     login_page = LoginPage(page)
@@ -16,16 +38,24 @@ def test_login_positive(page):
     login_page.login("aaaaa@wp.pl", "aaaaa")
 
     assert dashboard_page.is_user_logged_in(), "Login failed!"
-    
-    
-    
-    
+
+
+
+
 @pytest.mark.parametrize("username,password", [
     ("invalid@test.pl", "aaaaa"),
     ("aaaaa@wp.pl", "invalid")
 ])
 def test_login_invalid(page, username, password):
-    
+    """
+    Test login with invalid credentials.
+
+            Steps:
+            1. Navigate to the home page.
+            2. Go to the login page.
+            3. Attempt login with invalid credentials.
+            4. Assert that the appropriate error message is displayed.
+    """
     home = HomePage(page)
     login = LoginPage(page)
 
