95f46d10ee2b7fb43b126b132af74cc1ff5c5d54
szymonpilszak <szymon.pilszak@gmail.com>
2025-10-27 11:12:35 +0100

    Add test_login_positive.py

---
+++ tests/login/test_login_positive.py
@@ -0,0 +1,16 @@
+from pages.home_page import HomePage
+from pages.login_page import LoginPage
+from pages.dashboard_page import DashboardPage
+
+def test_login_success(page):
+    base_url = "https://tester.codersguru.pl/"
+
+    home_page = HomePage(page)
+    login_page = LoginPage(page)
+    dashboard_page = DashboardPage(page)
+
+    home_page.goto(base_url)
+    home_page.go_to_login_page()
+    login_page.login("aaaaa@wp.pl", "aaaaa")
+
+    assert dashboard_page.is_user_logged_in(), "Użytkownik nie został zalogowany!"
